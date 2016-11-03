2a0d525c7bfff391f7eaf8eeaf3b07a04858f174
石本利 <shibenli@vcredit.com>
2016-11-03 14:52:32 +0800

    添加基本的启动界面相关；

---
+++ living/src/main/java/com/vcredit/zj/noplayer/main/base/BaseActivity.java
@@ -0,0 +1,26 @@
+package com.vcredit.zj.noplayer.main.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shibenli on 2016/10/31.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
---
+++ living/src/main/java/com/vcredit/zj/noplayer/main/living/LivingActivity.java
@@ -0,0 +1,16 @@
+package com.vcredit.zj.noplayer.main.living;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vcredit.zj.noplayer.R;
+import com.vcredit.zj.noplayer.main.base.BaseActivity;
+
+public class LivingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_living);
+    }
+}
